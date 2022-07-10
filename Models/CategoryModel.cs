b6dc1ccdbef8eead935724b640831871c5dc890a
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-07-10 09:43:17 +0800

    initial project setup

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseRecorder.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseRecorder.Models
+{
+    public class CategoryModel
+    {
+        [Key] // we want this to be the primary key
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Icon { get; set; } = ""
+        [Column(TypeName = "nvarchar(50)")]
+        public string TransactionType { get; set; } = "Expense"
+
+
+    }
+}
---
+++ Models/TransactionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseRecorder.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public string? Note { get; set; } // this string is nullable
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
