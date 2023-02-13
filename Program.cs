bf04a1bd88b598aaccd88c463a26e656da90fa25
Andrewzhan2001 <97903569+Andrewzhan2001@users.noreply.github.com>
2023-02-14 00:39:18 +0800

    update connection stiring

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<WebDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+builder.Services.AddDbContext<WebDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 var app = builder.Build();
 
 //Register Syncfusion license
